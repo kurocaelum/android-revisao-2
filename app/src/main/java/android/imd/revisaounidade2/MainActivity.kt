35cf4ac17add37e2f2908c0f84b0c93fcbfdb0f1
Victor <victor_brandao@outlook.com>
2019-10-07 20:22:15 -0300

    First commit

---
+++ app/src/main/java/android/imd/revisaounidade2/MainActivity.kt
@@ -0,0 +1,13 @@
+package android.imd.revisaounidade2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// TODO Tela de login (referencia ao projeto Splash)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
